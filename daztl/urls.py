b13350fc67413e224121eadd0789154be6fdaca3
Zaid <zaidoskate@hotmail.com>
2025-06-13 10:55:58 -0600

    Correccion de la bd para carga de archivos, ahora se guardan urls, funciones para el home de android

--- daztl/urls.py
+++ daztl/urls.py
@@ -12,3 +12,6 @@ urlpatterns = [
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
